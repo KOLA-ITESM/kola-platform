2cd7152fbdb80cd52013a6ecde7a16cb6c7e9a2e
Sebastian Crossa <sebastiancrossa@hotmail.com>
2023-04-14 16:24:48 -0600

    feat: fetch for individual sensor info in /sensor/id route

--- src/pages/sensors/[id].js
+++ src/pages/sensors/[id].js
@@ -1,9 +1,37 @@
-const Sensor = () => {
+import { useRouter } from 'next/router'
+
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
+import CardHeader from '@mui/material/CardHeader'
+
+import prisma from '../../../prisma'
+
+const Sensor = ({ sensor }) => {
   return (
-    <div>
-      <h1>Sensor</h1>
-    </div>
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title={sensor.type} titleTypographyProps={{ variant: 'h6' }} />
+        </Card>
+      </Grid>
+    </Grid>
   )
+}
+
+export const getServerSideProps = async query => {
+  const sensorId = query.params.id
+  const sensor = await prisma.sensor.findUnique({
+    where: {
+      id: parseInt(sensorId)
+    }
+  })
+
+  return {
+    props: {
+      sensor
+    }
+  }
 }
 
 export default Sensor
