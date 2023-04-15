c6f5c2bec707e5f070f48f0c85188e5d3733b375
Sebastian Crossa <sebastiancrossa@hotmail.com>
2023-04-14 20:44:18 -0600

    feat: fetch sensor readings for individual sensor

--- src/pages/sensors/[id].js
+++ src/pages/sensors/[id].js
@@ -1,16 +1,29 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
 import { Typography } from '@mui/material'
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, TablePagination } from '@mui/material'
 
 import mapboxgl from 'mapbox-gl'
 import prisma from '../../../prisma'
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2ViZnJvbWxoIiwiYSI6ImNsZ2hkNmNodzAwMmkzZXA2cTJlMHlzY2UifQ.-0tFUeRnCr8jISRMn_CRvw'
 
-const Sensor = ({ sensor }) => {
+const Sensor = ({ sensor, sensorReadings }) => {
   const mapContainer = useRef()
+
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage)
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(+event.target.value)
+    setPage(0)
+  }
 
   useEffect(() => {
     const sensorCoordinates = [parseFloat(sensor.longitude), parseFloat(sensor.latitude)]
@@ -40,7 +53,37 @@ const Sensor = ({ sensor }) => {
         <div ref={mapContainer} style={{ width: '100%', height: '300px' }} className='rounded-lg'></div>
       </Grid>
       <Grid item xs={8}>
-        <Typography variant='h6'>Sensor Data</Typography>
+        <TableContainer>
+          <Table stickyHeader aria-label='sticky table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Date / Time</TableCell>
+                <TableCell>Reading</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {sensorReadings.map(sensorReading => (
+                <TableRow hover role='checkbox' tabIndex={-1} key={sensorReading.id} className='cursor-pointer'>
+                  <TableCell>{sensorReading.id}</TableCell>
+                  <TableCell>{sensorReading.createdAt}</TableCell>
+                  <TableCell>{sensorReading.reading}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <TablePagination
+          rowsPerPageOptions={[10]}
+          component='div'
+          count={sensorReadings}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </Grid>
     </Grid>
   )
@@ -54,9 +97,16 @@ export const getServerSideProps = async query => {
     }
   })
 
+  const sensorReadings = await prisma.sensorReading.findMany({
+    where: {
+      sensorId: parseInt(sensorId)
+    }
+  })
+
   return {
     props: {
-      sensor
+      sensor,
+      sensorReadings
     }
   }
 }
