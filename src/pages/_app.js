447131603bb1b43de23339e32169c04dd86a0c8b
Sebastian Crossa <sebastiancrossa@hotmail.com>
2023-04-14 20:39:07 -0600

    feat: show individual marker location

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,6 +26,7 @@ import 'react-perfect-scrollbar/dist/css/styles.css'
 
 // ** Global css styles
 import '../../styles/globals.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 const clientSideEmotionCache = createEmotionCache()
 
--- src/pages/sensors/[id].js
+++ src/pages/sensors/[id].js
@@ -1,19 +1,46 @@
-import { useRouter } from 'next/router'
+import { useEffect, useRef } from 'react'
 
 // ** MUI Imports
-import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
-import CardHeader from '@mui/material/CardHeader'
+import { Typography } from '@mui/material'
 
+import mapboxgl from 'mapbox-gl'
 import prisma from '../../../prisma'
 
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2ViZnJvbWxoIiwiYSI6ImNsZ2hkNmNodzAwMmkzZXA2cTJlMHlzY2UifQ.-0tFUeRnCr8jISRMn_CRvw'
+
 const Sensor = ({ sensor }) => {
+  const mapContainer = useRef()
+
+  useEffect(() => {
+    const sensorCoordinates = [parseFloat(sensor.longitude), parseFloat(sensor.latitude)]
+
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: sensorCoordinates,
+      zoom: 10
+    })
+
+    new mapboxgl.Marker().setLngLat(sensorCoordinates).addTo(map)
+
+    return () => {
+      map.remove()
+    }
+  }, [])
+
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        <Card>
-          <CardHeader title={sensor.type} titleTypographyProps={{ variant: 'h6' }} />
-        </Card>
+        <Typography variant='h5'>{sensor.type}</Typography>
+        <Typography variant='body2'>{sensor.location}</Typography>
+      </Grid>
+
+      <Grid item xs={4}>
+        <div ref={mapContainer} style={{ width: '100%', height: '300px' }} className='rounded-lg'></div>
+      </Grid>
+      <Grid item xs={8}>
+        <Typography variant='h6'>Sensor Data</Typography>
       </Grid>
     </Grid>
   )
